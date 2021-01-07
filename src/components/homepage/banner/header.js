6e7193e63fe66d304c2c8082ecbc43a335d5093e
rishabh verma <vermarishabh0987@gmail.com>
2021-01-07 13:12:22 +0530

    logout done

--- src/components/homepage/banner/header.js
+++ src/components/homepage/banner/header.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.scss";
 function Header() {
+  const [show, setShow] = useState(false);
   return (
     <div className="header">
       <h1>MyJobs</h1>
-      <button>Login/Signup</button>
-      {/* <div className="right">
-        <p className="applied">Applied Jobs</p>
-        <div className="avatar">
-          <p>P</p>
+
+      {localStorage.getItem("token") == null ? (
+        <button>Login/Signup</button>
+      ) : (
+        <div className="right">
+          <p className="applied">Applied Jobs</p>
+          <div className="avatar">
+            <p>{localStorage.getItem("name")[0].toUpperCase()}</p>
+          </div>
+          <p style={{ color: "red" }} onClick={() => setShow(!show)}>
+            down
+          </p>
+          {show ? (
+            <div style={{ position: "absolute", top: "110%", right: "0%" }}>
+              <button
+                className="logout"
+                onClick={() => {
+                  localStorage.clear();
+                }}
+              >
+                Logout
+              </button>
+            </div>
+          ) : null}
         </div>
-      </div> */}
+      )}
     </div>
   );
 }
