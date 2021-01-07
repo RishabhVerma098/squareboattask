271b22017afa3358ee61a66f46f0d945c9596d23
rishabh verma <vermarishabh0987@gmail.com>
2021-01-07 13:00:16 +0530

    login done

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -2,29 +2,72 @@ import React from "react";
 import "./login.scss";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../store/actions";
+import { useFormik } from "formik";
 function Login() {
   const dispatch = useDispatch();
-  const log = (e) => {
-    e.preventDefault();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => log(values),
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = "Email is required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Email invalid";
+      }
+
+      if (!values.password) {
+        errors.password = "Password is required";
+      } else if (values.password.length < 6) {
+        errors.password = "Password must alleast 6 char";
+      }
+
+      return errors;
+    },
+  });
+
+  const log = (values) => {
     dispatch(
       loginUser({
-        email: "sharad@gmail.com",
-        password: "sharad",
+        email: values.email,
+        password: values.password,
       })
     );
   };
   return (
     <>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label className="email">Email Address</label>
-        <input type="email" name="email" placeholder="Enter your email"></input>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+        ></input>
+        {formik.touched.email && formik.errors.email ? (
+          <div className="error">{formik.errors.email}</div>
+        ) : null}
         <label className="password">Password</label>
         <input
           type="password"
           name="password"
           placeholder="Enter your password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          onBlur={formik.handleBlur}
         ></input>
+        {formik.touched.password && formik.errors.password ? (
+          <div className="error">{formik.errors.password}</div>
+        ) : null}
         <button type="submit" onClick={(e) => log(e)}>
           Login
         </button>
